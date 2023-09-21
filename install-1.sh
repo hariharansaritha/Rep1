b6febf50eb936ae94e47d648fbc428eb553a18c6
hariharansaritha <48622342+hariharansaritha@users.noreply.github.com>
2023-09-21 18:17:19 +1000

    Create install-1.sh

---
+++ install-1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$EUID" -eq 0 ]; then
+  echo "Please do not run this script as root."
+  exit
+fi
+echo -e "\n\nInstalling system dependencies..."
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y libwebkit2gtk-4.0-dev build-essential curl wget libssl-dev libgtk-3-dev libayatana-appindicator3-dev librsvg2-dev
+echo -e "\n\nInstalling Rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+echo -e "\n\nInstalling Volta..."
+curl https://get.volta.sh | bash
+echo -e "\n\nInstallation of initial dependencies complete.\n"
+chmod +x install-2.sh
+./install-2.sh
