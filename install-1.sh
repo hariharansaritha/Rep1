e02eb404f49e3b355b81c554a17395c057b1ce44
hariharansaritha <48622342+hariharansaritha@users.noreply.github.com>
2023-09-22 02:31:00 +1000

    Update install-1.sh

--- install-1.sh
+++ install-1.sh
@@ -12,6 +12,7 @@ while true; do
       echo "Updating System"
       echo "-----------------------------"
       sudo apt update && sudo apt upgrade -y
+      echo -e "\n Done..."
       break;;
     [Nn]* )
       break;;
@@ -19,21 +20,25 @@ while true; do
       echo "Invalid input. Please enter y or n.";;
   esac
 done
-echo "-----------------------------"
-echo -e "\n\nInstalling dependencies"
+echo "\n\n-----------------------------"
+echo -e "Installing dependencies"
 echo "-----------------------------"
 sudo apt install -y libwebkit2gtk-4.0-dev build-essential curl wget libssl-dev libgtk-3-dev libayatana-appindicator3-dev librsvg2-dev python3-impacket
-echo "-----------------------------"
-echo -e "\n\nInstalling Rust"
+echo -e "\n Done..."
+echo "\n\n-----------------------------"
+echo -e "Installing Rust"
 echo "-----------------------------"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-echo "-----------------------------"
-echo -e "\n\nInstalling Volta"
+echo -e "\n Done..."
+echo "\n\n-----------------------------"
+echo -e "Installing Volta"
 echo "-----------------------------"
 curl https://get.volta.sh | bash
+echo -e "\n Done..."
+echo "\n\n-------------------------------------------------------------"
+echo -e "Installation of initial dependencies complete.\n"
 echo "-------------------------------------------------------------"
-echo -e "\n\nInstallation of initial dependencies complete.\n"
-echo "-------------------------------------------------------------"
+echo -e "\n Done..."
 echo -e "\n\n"
 echo "Fetching and executing the second installation script..."
 curl -sSL https://raw.githubusercontent.com/hariharansaritha/Rep1/master/install2.sh -o install2.sh && bash install2.sh
