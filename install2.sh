4794b0a8433174df08ad4a9ed3f9207ca9efd7a5
hariharansaritha <48622342+hariharansaritha@users.noreply.github.com>
2023-09-22 02:33:24 +1000

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
-echo -e "\n\n"
-echo "-----------------------------"
-echo -e "\n\nConfiguring Volta..."
+echo "\n\n-----------------------------"
+echo -e "Configuring Volta..."
 echo "-----------------------------"
 source ~/.zshrc > /dev/null 2>&1
-echo -e "\n\n"
-echo "-----------------------------"
+echo -e "\n Done..."
+echo "\n\n-----------------------------"
 echo -e "\n\nInstalling Node.js"
 echo "-----------------------------"
 volta install node
-echo -e "\n\n"
-echo "-----------------------------"
-echo -e "\n\nInstalling Yarn"
+echo -e "\n Done..."
+echo "\n\n-----------------------------"
+echo -e "Installing Yarn"
 echo "-----------------------------"
 volta install yarn
+echo -e "\n Done..."
 echo -e "\n\n"
 read -p "Please specify a location to clone the repository (or press Enter for default location): " repo_location
 if [ -z "$repo_location" ]; then
   repo_location=$HOME
 fi
 git clone https://github.com/Hardhat-Enterprises/Deakin-Detonator-Toolkit $repo_location/Deakin-Detonator-Toolkit
-echo "-----------------------------------------------"
-echo -e "\n\nInstalling project dependencies"
+echo "\n\n-----------------------------------------------"
+echo -e "Installing project dependencies"
 echo "-----------------------------------------------"
 sudo apt install -y mitmproxy libglib2.0-dev libsoup2.4-dev libjavascriptcoregtk-4.0-18 libjavascriptcoregtk-4.0-dev libwebkit2gtk-4.1-0 libwebkit2gtk-4.1-dev openjdk-11-jdk cargo nodejs dsniff enum4linux dnsmap goldeneye wpscan parsero arjun sherlock
 cd $repo_location/Deakin-Detonator-Toolkit
 yarn install
-echo "-----------------------------"
-echo -e "\n\nInstalling Exploits"
+echo -e "\n Done..."
+echo "\n\n-----------------------------"
+echo -e "Installing Exploits"
 echo "-----------------------------"
 sudo mkdir -p /usr/share/ddt/
 sudo cp -r src-tauri/exploits/* /usr/share/ddt/
+echo -e "\n Done..."
 while true; do
   echo -e "\n\nStarting DDT..."
   read -p "Do you want to start DDT now? (y/n) " start_ddt
