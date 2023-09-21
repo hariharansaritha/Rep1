20443154b00ad8a51fcf64afb3d7b4862f31b31c
hariharansaritha <48622342+hariharansaritha@users.noreply.github.com>
2023-09-22 02:23:59 +1000

    Update install-1.sh

--- install-1.sh
+++ install-1.sh
@@ -8,7 +8,9 @@ while true; do
   read -p "Do you want to update your system now? - Recommended (y/n) " start_update
   case $start_update in
     [Yy]* )
-      echo "Updating System..."
+      echo "-----------------------------"
+      echo "Updating System"
+      echo "-----------------------------"
       sudo apt update && sudo apt upgrade -y
       break;;
     [Nn]* )
@@ -17,12 +19,21 @@ while true; do
       echo "Invalid input. Please enter y or n.";;
   esac
 done
+echo "-----------------------------"
 echo -e "\n\nInstalling dependencies"
+echo "-----------------------------"
 sudo apt install -y libwebkit2gtk-4.0-dev build-essential curl wget libssl-dev libgtk-3-dev libayatana-appindicator3-dev librsvg2-dev python3-impacket
-echo -e "\n\nInstalling Rust..."
+echo "-----------------------------"
+echo -e "\n\nInstalling Rust"
+echo "-----------------------------"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-echo -e "\n\nInstalling Volta..."
+echo "-----------------------------"
+echo -e "\n\nInstalling Volta"
+echo "-----------------------------"
 curl https://get.volta.sh | bash
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo -e "\n\nInstallation of initial dependencies complete.\n"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo -e "\n\n"
 echo "Fetching and executing the second installation script..."
 curl -sSL https://raw.githubusercontent.com/hariharansaritha/Rep1/master/install2.sh -o install2.sh && bash install2.sh
