0084bf341f1502fcde9bd92f9b9feb4971c501d3
hariharansaritha <48622342+hariharansaritha@users.noreply.github.com>
2023-09-22 02:40:45 +1000

    Update install-1.sh

--- install-1.sh
+++ install-1.sh
@@ -12,7 +12,7 @@ while true; do
       echo -e "Updating System"
       echo -e "-----------------------------"
       sudo apt update && sudo apt upgrade -y
-      echo -e "\n Done..."
+      echo -e "\nDone..."
       break;;
     [Nn]* )
       break;;
@@ -24,21 +24,21 @@ echo -e "\n\n-----------------------------"
 echo -e "Installing dependencies"
 echo -e "-----------------------------"
 sudo apt install -y libwebkit2gtk-4.0-dev build-essential curl wget libssl-dev libgtk-3-dev libayatana-appindicator3-dev librsvg2-dev python3-impacket
-echo -e "\n Done..."
+echo -e "\nDone..."
 echo -e "\n\n-----------------------------"
 echo -e "Installing Rust"
 echo -e "-----------------------------"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-echo -e "\n Done..."
+echo -e "\nDone..."
 echo -e "\n\n-----------------------------"
 echo -e "Installing Volta"
 echo -e "-----------------------------"
 curl https://get.volta.sh | bash
-echo -e "\n Done..."
+echo -e "\nDone..."
 echo -e "\n\n-------------------------------------------------------------"
 echo -e "Installation of initial dependencies complete.\n"
 echo -e "-------------------------------------------------------------"
-echo -e "\n Done..."
+echo -e "\nDone..."
 echo -e "\n\n"
 echo -e "Fetching and executing the second installation script..."
 curl -sSL https://raw.githubusercontent.com/hariharansaritha/Rep1/master/install2.sh -o install2.sh && bash install2.sh
