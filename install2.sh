e87dd0972f2e840b03bdf86fd5ad5311ace4779c
hariharansaritha <48622342+hariharansaritha@users.noreply.github.com>
2023-09-25 21:16:00 +1000

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -23,7 +23,7 @@ git clone https://github.com/Hardhat-Enterprises/Deakin-Detonator-Toolkit $repo_
 echo -e "\n\n-----------------------------------------------"
 echo -e "Installing project dependencies"
 echo -e "-----------------------------------------------"
-sudo apt install -y mitmproxy libglib2.0-dev libsoup2.4-dev libjavascriptcoregtk-4.0-18 libjavascriptcoregtk-4.0-dev libwebkit2gtk-4.1-0 libwebkit2gtk-4.1-dev openjdk-11-jdk cargo nodejs dsniff enum4linux dnsmap goldeneye wpscan parsero arjun sherlock snmpcheck dirb hashcat hydra dnsenum enum4linux crunch dmitry cewl netcat-openbsd dnsrecon bed crackmapexec wpscan gobuster
+sudo apt install -y mitmproxy libglib2.0-dev libsoup2.4-dev libjavascriptcoregtk-4.0-18 libjavascriptcoregtk-4.0-dev libwebkit2gtk-4.1-0 libwebkit2gtk-4.1-dev openjdk-11-jdk cargo nodejs dsniff enum4linux dnsmap goldeneye wpscan parsero arjun sherlock
 pip install shodan
 git clone https://github.com/FortyNorthSecurity/EyeWitness.git && cd EyeWitness && sudo ./setup/setup.sh && cd .. && \
 git clone https://github.com/ffuf/ffuf.git && cd ffuf && go get && go build && cd .. && \
