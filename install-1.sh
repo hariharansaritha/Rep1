ff37817705ad9759a59db3ceb45be49f10272c84
hariharansaritha <48622342+hariharansaritha@users.noreply.github.com>
2023-09-26 00:41:01 +1000

    Update install-1.sh

--- install-1.sh
+++ install-1.sh
@@ -23,7 +23,12 @@ done
 echo -e "\n\n-----------------------------"
 echo -e "Installing dependencies"
 echo -e "-----------------------------"
-sudo apt install -y libwebkit2gtk-4.0-dev build-essential curl wget libssl-dev libgtk-3-dev libayatana-appindicator3-dev librsvg2-dev python3-impacket
+sudo apt install -y libwebkit2gtk-4.0-dev build-essential curl wget libssl-dev libgtk-3-dev libayatana-appindicator3-dev librsvg2-dev python3-impacket mitmproxy libglib2.0-dev libsoup2.4-dev libjavascriptcoregtk-4.0-18 libjavascriptcoregtk-4.0-dev libwebkit2gtk-4.1-0 libwebkit2gtk-4.1-dev openjdk-11-jdk cargo nodejs dsniff enum4linux dnsmap goldeneye wpscan parsero arjun sherlock
+pip install shodan
+git clone https://github.com/FortyNorthSecurity/EyeWitness.git && cd EyeWitness && sudo ./setup/setup.sh && cd .. && \
+git clone https://github.com/ffuf/ffuf.git && cd ffuf && go get && go build && cd .. && \
+git clone https://github.com/laramies/theHarvester.git && cd theHarvester && pip install -r requirements.txt && cd .. && \
+git clone https://github.com/laramies/metagoofil.git && cd metagoofil && pip install -r requirements.txt && cd ..
 echo -e "\nDone..."
 echo -e "\n\n-----------------------------"
 echo -e "Installing Rust"
