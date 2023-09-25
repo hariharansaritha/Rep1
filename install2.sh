a40075fda3b022c2ed340390f66119b6ccee9f31
hariharansaritha <48622342+hariharansaritha@users.noreply.github.com>
2023-09-26 00:40:29 +1000

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -20,15 +20,6 @@ if [ -z "$repo_location" ]; then
   repo_location=$HOME
 fi
 git clone https://github.com/Hardhat-Enterprises/Deakin-Detonator-Toolkit $repo_location/Deakin-Detonator-Toolkit
-echo -e "\n\n-----------------------------------------------"
-echo -e "Installing project dependencies"
-echo -e "-----------------------------------------------"
-sudo apt install -y mitmproxy libglib2.0-dev libsoup2.4-dev libjavascriptcoregtk-4.0-18 libjavascriptcoregtk-4.0-dev libwebkit2gtk-4.1-0 libwebkit2gtk-4.1-dev openjdk-11-jdk cargo nodejs dsniff enum4linux dnsmap goldeneye wpscan parsero arjun sherlock
-pip install shodan
-git clone https://github.com/FortyNorthSecurity/EyeWitness.git && cd EyeWitness && sudo ./setup/setup.sh && cd .. && \
-git clone https://github.com/ffuf/ffuf.git && cd ffuf && go get && go build && cd .. && \
-git clone https://github.com/laramies/theHarvester.git && cd theHarvester && pip install -r requirements.txt && cd .. && \
-git clone https://github.com/laramies/metagoofil.git && cd metagoofil && pip install -r requirements.txt && cd ..
 cd $repo_location/Deakin-Detonator-Toolkit
 yarn install
 echo -e "\n\nCleaning up..."
