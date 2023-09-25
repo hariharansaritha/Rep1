64b9960bd6c41f260024d56b1e587bb381394220
hariharansaritha <48622342+hariharansaritha@users.noreply.github.com>
2023-09-25 19:03:40 +1000

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -24,6 +24,11 @@ echo -e "\n\n-----------------------------------------------"
 echo -e "Installing project dependencies"
 echo -e "-----------------------------------------------"
 sudo apt install -y mitmproxy libglib2.0-dev libsoup2.4-dev libjavascriptcoregtk-4.0-18 libjavascriptcoregtk-4.0-dev libwebkit2gtk-4.1-0 libwebkit2gtk-4.1-dev openjdk-11-jdk cargo nodejs dsniff enum4linux dnsmap goldeneye wpscan parsero arjun sherlock snmpcheck dirb hashcat hydra dnsenum enum4linux crunch dmitry cewl netcat dnsrecon bed crackmapexec wpscan gobuster
+pip install shodan
+git clone https://github.com/FortyNorthSecurity/EyeWitness.git && cd EyeWitness && sudo ./setup/setup.sh && cd .. && \
+git clone https://github.com/ffuf/ffuf.git && cd ffuf && go get && go build && cd .. && \
+git clone https://github.com/laramies/theHarvester.git && cd theHarvester && pip install -r requirements.txt && cd .. && \
+git clone https://github.com/laramies/metagoofil.git && cd metagoofil && pip install -r requirements.txt && cd ..
 cd $repo_location/Deakin-Detonator-Toolkit
 yarn install
 echo -e "\n\nCleaning up..."
